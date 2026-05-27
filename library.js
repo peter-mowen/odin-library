0ad993524bfa4e1efc2a9ef2847674223426f482
Peter Mowen <peter.m.mowen@gmail.com>
2026-05-27 12:06:21 -0400

    Add "Add Book" modal and first round of styling

--- library.js
+++ library.js
@@ -20,3 +20,29 @@ theSunlitMan = new Book("The Sunlit Man", "Brandon Sanderson", 400, false);
 
 // Fill the library with sample books
 myLibrary = [windAndTruth, theMismeasureOfMan, aChristmasCarol, commonSenseInvesting, arcanumUnbounded, theSunlitMan];
+
+// Get the modal
+var modal = document.getElementById("add-book-dialog");
+
+// Get the button that opens the modal
+var btn = document.getElementById("add-book-btn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("add-book-modal-close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
